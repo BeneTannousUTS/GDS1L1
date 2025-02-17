8e28c5bfcb6a90f002215f08e7a11e5ac468cb75
Benedict Tannous laptop <benedict.m.tannous@student.uts.edu.au>
2025-02-17 16:42:49 +1100

    Collisions completed

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,8 @@ public class MovementScript : MonoBehaviour
     public float MoveSpeed = 5.0f;
     private Vector2 Movement;
     private Rigidbody2D rb;
+    protected int SoldiersOnBoard = 0;
+    protected int SoldiersSaved = 0;
 
     void Start()
     {
@@ -24,4 +27,43 @@ public class MovementScript : MonoBehaviour
         // Apply movement
         rb.velocity = Movement * MoveSpeed;
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var collidingTag = other.gameObject.tag;
+        switch (collidingTag)
+        {
+            case "Homebase":
+                //drop soldiers off
+                if (SoldiersOnBoard > 0)
+                {
+                    SoldiersOnBoard = 0;
+                    SoldiersSaved += SoldiersOnBoard;
+                    break;
+                }
+                
+                Debug.Log("No Soldiers to drop off! Save some soldiers and try again.");
+                break;
+            case "Obstacle":
+                //Gameover Screen
+                Debug.Log("Game Over");
+                break;
+            case "Soldier":
+                if (SoldiersOnBoard < 3)
+                {
+                    Destroy(other.gameObject);
+                    SoldiersOnBoard += 1;
+                    Debug.Log("Soldier Saved! You have " + SoldiersOnBoard + " soldiers on board now.");
+                    break;
+                }
+                
+                Debug.Log("Too many soldiers on board! Take them to the Hospital and try again.");
+                break;
+            default:
+                Debug.Log("Not recognised collision");
+                break;
+        }
+    }
+    
+    
 }
